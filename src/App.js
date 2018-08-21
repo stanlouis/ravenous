0386124e6012dbaa8bc14d12fdcc8f074512ac2f
Stanley Louis <stanlouis@msn.com>
2018-08-21 07:51:40 -0400

    Add SearchBar Component and Style, wire up all components in app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import React, { Component } from 'react';
-import Business from './components/Business/Business';
+import BusinessList from './components/BusinessList/BusinessList';
+import SearchBar from './components/SearchBar/SearchBar';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Business/>
+        <SearchBar />
+        <BusinessList />
       </div>
     );
   }
 }
-
-export default App;
